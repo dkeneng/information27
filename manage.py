05c446f50eba86fb4fda6f469029a370a1a998c0
diaoxianyi <diaoxinayi@163.com>
2021-07-05 20:17:22 +0800

    添加manager管理模块和migrate数据迁移模块

--- manage.py
+++ manage.py
@@ -1,8 +1,18 @@
-from info import create_app
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+
+from info import create_app, db
 
 app = create_app("development")
+
+manage = Manager(app)
+# 将app与db关联
+Migrate(app, db)
+# 将迁移命令添加到manager中
+manage.add_command('db', MigrateCommand)
 
 
 if __name__ == '__main__':
     print(app.url_map)
-    app.run()
+    # app.run()
+    manage.run()
